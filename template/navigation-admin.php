4583fd91e1da9377519d3d89a14fdb0454e4a099
alamshertiwana <45050712+alamshertiwana@users.noreply.github.com>
2022-05-11 21:26:22 +0100

    Removed swimmer squad menu

--- template/navigation-admin.php
+++ template/navigation-admin.php
@@ -39,12 +39,6 @@
           Add Gala
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="add-swimmer-squad.php">
-          <span data-feather="plus-circle"></span>
-          Assign Swimmers Squad
-        </a>
-      </li>
     </ul>
     
     <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
