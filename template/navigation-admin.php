20da19f17c98cf73950e02f4dcc65712e6532434
alamshertiwana <45050712+alamshertiwana@users.noreply.github.com>
2022-05-11 21:04:48 +0100

    Updated admin navigation

--- template/navigation-admin.php
+++ template/navigation-admin.php
@@ -1,75 +1,70 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              <span data-feather="home"></span>
-              Dashboard
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="search-users.php">
-              <span data-feather="filter"></span>
-              Search & Filter
-            </a>
-          </li>          
-        </ul>
+  <div class="position-sticky pt-3">
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Add Items</span>
-        </h6>
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">
+          <span data-feather="home"></span>
+          Dashboard
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="search-users.php">
+          <span data-feather="filter"></span>
+          Search & Filter
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="view-gala.php">
+          <span data-feather="flag"></span>
+          View Gala
+        </a>
+      </li>                
+    </ul>
 
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link" href="add-squad.php">
-              <span data-feather="plus-circle"></span>
-              Add Squad
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="add-gala.php">
-              <span data-feather="plus-circle"></span>
-              Add Gala
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="add-swimmer-squad.php">
-              <span data-feather="plus-circle"></span>
-              Assign Swimmers Squad
-            </a>
-          </li>
-        </ul>
-        
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>View Items</span>
-        </h6>
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+      <span>Add Items</span>
+    </h6>
 
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link" href="view-users.php">
-              <span data-feather="plus-circle"></span>
-              View Users
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="view-coaches.php">
-              <span data-feather="plus-circle"></span>
-              View Coaches
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="view-gala.php">
-              <span data-feather="plus-circle"></span>
-              View Gala
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="view-squads.php">
-              <span data-feather="plus-circle"></span>
-              View Squads
-            </a>
-          </li>
-        </ul>          
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="add-squad.php">
+          <span data-feather="plus-circle"></span>
+          Add Squad
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="add-gala.php">
+          <span data-feather="plus-circle"></span>
+          Add Gala
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="add-swimmer-squad.php">
+          <span data-feather="plus-circle"></span>
+          Assign Swimmers Squad
+        </a>
+      </li>
+    </ul>
+    
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+      <span>View Items</span>
+    </h6>
 
-      </div>
-    </nav>
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="view-users.php">
+          <span data-feather="plus-circle"></span>
+          View Users
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="view-coaches.php">
+          <span data-feather="plus-circle"></span>
+          View Coaches
+        </a>
+      </li>
+    </ul>          
+
+  </div>
+</nav>
